498a0414bac880849397a72b4e94cd0f078fcdc0
Ramana <ramanareddyk_selenium@live.in>
2020-08-19 09:40:59 +0530

    This is my first commit :)

---
+++ git_practice_01/src/main/java/Git/git_practice_01/App.java
@@ -0,0 +1,13 @@
+package Git.git_practice_01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ git_practice_01/src/test/java/Git/git_practice_01/Sample.java
@@ -0,0 +1,11 @@
+package Git.git_practice_01;
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		System.out.println("Git Smaple Code stareted with Poject _19:32");
+
+	}
+
+}
